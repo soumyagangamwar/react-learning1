42c824169eb83d261a93af94db83a45e4bff7f2b
soumyagangamwar <soumyagangawar@gmail.com>
2020-01-04 19:23:19 +0530

    Updates

---
+++ precache-manifest.a95feb92cad27bf7c24b4acc4d05955b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99085732a462b7aa03b0ca1dd08b900b",
+    "url": "/react-learning1/index.html"
+  },
+  {
+    "revision": "aca296a5fbb51fe340f4",
+    "url": "/react-learning1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9f6d53959311150317d",
+    "url": "/react-learning1/static/js/2.88600d35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-learning1/static/js/2.88600d35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aca296a5fbb51fe340f4",
+    "url": "/react-learning1/static/js/main.400ee9f0.chunk.js"
+  },
+  {
+    "revision": "dbd28d4b5183d2ab155c",
+    "url": "/react-learning1/static/js/runtime-main.0baa730c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-learning1/static/media/logo.5d5d9eef.svg"
+  }
+]);
