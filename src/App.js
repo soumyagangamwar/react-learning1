0503f3b8997b85b4c5511dd38a27de12ee0bef9f
soumyagangamwar <soumyagangawar@gmail.com>
2020-01-05 12:43:59 +0530

    Functional componets with export

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        
-        <MyComponent></MyComponent>
-         
-       
+        <MyComponent></MyComponent>     
       </header>
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,7 +3,6 @@ import React from 'react';
 // function Greet () {
 //     return <h1>Hello Soumya</h1>
 // }
-
 const Greet = () => <h1>Hello Soumy Gangamwar</h1>
 
 export default Greet;
